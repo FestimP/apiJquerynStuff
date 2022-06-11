0af604c0a7f28fa7b213033079cb4c6238c50f07
qendrim imeri <qendrim.s.imeri@gmail.com>
2022-06-11 11:04:27 +0200

    sus

--- SQL-DB/storedProcedures.sql
+++ SQL-DB/storedProcedures.sql
@@ -1,3 +1,4 @@
+--Stored Procedures---
 SELECT * FROM Personi p
 WHERE p.Mosha>18
 
@@ -32,8 +33,17 @@ AS
 	SELECT * FROM Personi u 
 	INNER JOIN Gjinia g on g.id=u.GenderId
 	INNER JOIN Roles r on r.id=u.RoleId
-	WHERE u.Mosha>21
+	WHERE u.Mosha>21;
+
+	--return(SELECT COUNT(u.id); 
+	--FROM Personi u 
+	--INNER JOIN Gjinia g on g.id=u.GenderId
+	--INNER JOIN Roles r on r.id=u.RoleId
+	--WHERE u.Mosha>21);
 GO
+--declare @pReturnType int;
+--exec @pReturnType = people.spGetPeopleInfos
+--select @pReturnType as [vlera kthyese]
 
 EXEC people.spGetPeopleInfos
 
@@ -76,3 +86,63 @@ AS
 GO
 
 EXEC spGetUsersByAgeName 23, 'Filan';
+
+
+Create PROCEDURE people.spGetPersonInfosAndRowCount
+@pAge int , @pRowCount int OUT 
+AS
+	SELECT u.ID,
+		   u.Emri,
+		   u.Mbimeri,
+		   g.Description  AS Gjinia,
+		   r.Emri AS Profesioni
+	FROM users u 
+	inner join Genders g ON g.id=u.Gjinia
+	inner join Roles r ON r.id=u.RoleId
+	WHERE u.mosha>@pAge;
+
+	select @pRowCount=Count(*)
+	FROM users u 
+	inner join Genders g ON g.id=u.Gjinia
+	inner join Roles r ON r.id=u.RoleId
+		WHERE u.mosha>@pAge;
+
+Go
+Declare @oReturnValue int;
+exec people.spGetPersonInfosAndRowCount 21,@oReturnValue out;
+select @oReturnValue as [numri i rreshtave]
+select 1;
+go
+
+alter PROCEDURE people.spGetNameByID
+@Id int , @Emri nvarchar(100) out,@Mosha int out
+AS 
+SELECT @Emri=u.Emri,
+@Mosha=u.mosha
+	FROM users u 
+	WHERE u.Id=@Id;
+	go
+Declare @id int=3;
+Declare @Emri nvarchar(100);
+Declare @Mosha int;
+exec people.spGetNameByID @id,@Emri OUT, @Mosha OUTPUT;
+Select @Emri as emri, @mosha as mosha;
+print @Emri + '' + Cast(@mosha as nvarchar(10));
+select 1;
+-- Return type - e kthen 1 vlere 
+-- output type - mun kthen shum
+declare @Person Table(id int,Emri varchar(40),Mbiemri varchar(40));
+insert into @Person
+values(1,'kreshnik','gashi');
+select * from @Person
+drop table if Exists #TempPerson 
+select * 
+into #TempPerson
+from @Person
+select * from #TempPerson
+-- STOCK procedurat jan ma te mira(vs me queryt ) sepse:
+-- stock proceduat ruhen edhe ski nevoj me i deklaru saher qe don me perdor
+-- it is better per response time
+-- metod ma e shpejt(ni her procesohet tana ta shfaq rezultatin)
+-- ma i sigurt,munesh me ba empose restrictions, me e rregullu qasjen,abstrahimi etj etj.
+-- e pamundeson sql injecition 
